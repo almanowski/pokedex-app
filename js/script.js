8898364b4cb80ae634cffcd0e31e6f646fcf7d89
Anna Manowski <anna.l.manowski@gmail.com>
2021-10-28 09:44:32 +0200

    Create a new variable with an array that contains Pokémon data
    New exercise with arrays and objects.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-
+const pokemonList = [{
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Butterfree',
+        height: 1.1,
+        types: ['bug', 'flying']
+    },
+    {
+        name: 'Eevee',
+        height: 0.3,
+        types: ['normal']
+    }
+]
