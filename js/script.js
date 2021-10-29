6d5b2d5f31e1c2ec81dd52bc7a325b820d1ea8d4
Anna Manowski <anna.l.manowski@gmail.com>
2021-10-29 16:14:38 +0200

    Add loop and conditional for pokemonList

--- js/script.js
+++ js/script.js
@@ -13,4 +13,29 @@ const pokemonList = [{
         height: 0.3,
         types: ['normal']
     }
-]
+];
+
+
+//Loop for pokemonlist array
+for (let i = 0; i < pokemonList.length; i++) {
+    //if-else - adding comments about pokemon height
+    if (pokemonList[i].height > 1.0) {
+        //div, h2, p for css
+        document.write('<div class="pokemonlist__item">' +
+            '<h2>' + pokemonList[i].name + '</h2>' +
+            '<p>' + 'Height: ' + pokemonList[i].height +
+            ' - Wow, that\'s big!! ' + '</p>' +
+            '<p>' + 'Type: ' + pokemonList[i].types + '</p>' + '</div>')
+    } else if (pokemonList[i].height > 0.5 && pokemonList[i].height < 1.0) {
+        document.write('<div class="pokemonlist__item">' +
+            '<h2>' + pokemonList[i].name + '</h2>' +
+            '<p>' + 'Height: ' + pokemonList[i].height + '</p>' +
+            '<p>' + 'Type: ' + pokemonList[i].types + '</p>' + '</div>')
+    } else {
+        document.write('<div class="pokemonlist__item">' +
+            '<h2>' + pokemonList[i].name + '</h2>' +
+            '<p>' + 'Height: ' + pokemonList[i].height +
+            ' - Smoll baby! ' + '</p>' + '<p>' + 'Type: ' +
+            pokemonList[i].types + '</p>' + '</div>')
+    }
+}
