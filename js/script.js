6dec690141354f3ae7e43c50ce97c278791858b4
Anna Manowski <anna.l.manowski@gmail.com>
2021-11-17 16:33:52 +0100

    Add function for scroll Button

--- js/script.js
+++ js/script.js
@@ -150,3 +150,30 @@ pokemonRepository.loadList().then(function() {
         pokemonRepository.addListPokemon(pokemon);
     });
 });
+
+
+
+
+//Add scroll to top Button
+const scrollButton = document.querySelector('.top-button');
+
+//When the user scrolls down 20px from the top of the document, show the Button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = 'block';
+  } else {
+    scrollButton.style.display = 'none';
+  }
+}
+
+//When the user clicks on the button, scroll to the top of the documentElement
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+scrollButton.addEventListener('click', function() {
+    topFunction();
+});
